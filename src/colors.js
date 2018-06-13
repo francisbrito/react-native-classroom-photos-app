d3eda0e968f717fdc27126f4e89745b17c8e9483
Francis Brito <francis@instacarro.com>
2018-06-13 14:57:19 -0400

    feat: Change color palette to blue

--- src/colors.js
+++ src/colors.js
@@ -1,7 +1,7 @@
-export const DARK_PRIMARY_COLOR = '#1976D2';
-export const LIGHT_PRIMARY_COLOR = '#BBDEFB';
-export const PRIMARY_COLOR = '#2196F3';
+export const DARK_PRIMARY_COLOR = '#303F9F';
+export const LIGHT_PRIMARY_COLOR = '#C5CAE9';
+export const PRIMARY_COLOR = '#3F51B5';
 export const ICON_COLOR = '#FFFFFF';
 export const TEXT_COLOR = '#FFFFFF';
-export const ACCENT_COLOR = '#FF4081';
-export const SECONDARY_TEXT_COLOR = '#c5c5c5';
+export const ACCENT_COLOR = '#03A9F4';
+export const SECONDARY_TEXT_COLOR = '#757575';
